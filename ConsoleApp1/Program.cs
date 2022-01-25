7c4f036952aaf80a22ea715b31c7c8067d17c289
Sergei Khramtsov <vocmarh1989@gmail.com>
2022-01-25 21:03:38 +0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab21
+{
+    class Program
+    {
+        private static int x;
+        private static int y;
+        private static int[,] pole;
+
+        static void Main()
+        {
+            Console.WriteLine("Vvedite razmer polya:");
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            pole = new int[x, y];
+            ThreadStart threadStart = new ThreadStart(Sadovnik1);
+            Thread thread = new Thread(threadStart);
+            ThreadStart threadStart1 = new ThreadStart(Sadovnik2);
+            Thread thread2 = new Thread(threadStart1);
+            thread.Start();
+            thread2.Start();
+            thread.Join();
+            thread2.Join();
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.Write(pole[i, j] + " ");
+
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+        static void Sadovnik1()
+        {
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    if (pole[i, j] == 0)
+                    {
+                        pole[i, j] = 1;
+                        Thread.Sleep(5);
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Sadovnik2()
+        {
+            for (int j = y - 1; j > 0; j--)
+            {
+                for (int i = x - 1; i > 0; i--)
+                {
+                    if (pole[j, i] == 0)
+                    {
+                        pole[j, i] = 2;
+                        Thread.Sleep(5);
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Threading/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    class Program
+    {
+        private static int[,] Field;
+        private static int TheGardener1;
+        private static int TheGardener2;
+        static object locker = new object();
+        static void Main()
+        {
+            Console.WriteLine("Введите первую сторону участка: ");
+            int Side1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите вторую сторону участка: ");
+            int Side2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Садовники приступили к работе");
+            TheGardener1 = Side1;
+            TheGardener2 = Side2;
+            Field = new int[Side1, Side2];
+            Thread TheGardenerWork1 = new Thread(Sad1);
+            Thread TheGardenerWork2 = new Thread(Sad2);
+            TheGardenerWork1.Start();
+            TheGardenerWork2.Start();
+            Console.ReadKey();
+        }
+        private static void Sad1()
+        {
+            lock (locker)
+                for (int i = 0; i < TheGardener2; i++)
+                {
+                    for (int j = 0; j < TheGardener1; j++)
+                    {
+                        if (Field[i, j] == 0)
+                            Field[i, j] = 1;
+                        Console.Write(Field[i, j] + " ");
+                        Console.WriteLine();
+                        Thread.Sleep(100);
+                    }
+                }
+        }
+        private static void Sad2()
+        {
+            for (int i = TheGardener1 - 1; i > 0; i--)
+            {
+                for (int j = TheGardener2 - 1; j > 0; j--)
+                {
+                    if (Field[j, i] == 0)
+                        Field[j, i] = 2;
+                    Console.Write(Field[i, j] + " ");
+                    Thread.Sleep(100);
+                }
+            }
+        }
+    }
+}
